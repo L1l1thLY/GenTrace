02d8d0a11dd36cf2614e13975402ae349a258f9d
Lilith-Sangreal <chibaibuki@outlook.com>
2019-02-22 10:34:50 +0800

    Initialized

---
+++ src/main/java/com/uestc/lilith/gentrace/GentraceApplication.java
@@ -0,0 +1,13 @@
+package com.uestc.lilith.gentrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GentraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uestc/lilith/gentrace/GentraceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uestc.lilith.gentrace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GentraceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
