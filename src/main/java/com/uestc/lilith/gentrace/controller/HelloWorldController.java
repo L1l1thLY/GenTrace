cfdd3ad4fd1b905a44e8cfec75cc99e9fed67a39
Lilith-Sangreal <chibaibuki@outlook.com>
2019-03-13 09:45:44 +0800

    Initialized

--- src/main/java/com/uestc/lilith/gentrace/controller/HelloWorldController.java
+++ src/main/java/com/uestc/lilith/gentrace/controller/HelloWorldController.java
@@ -3,10 +3,19 @@ package com.uestc.lilith.gentrace.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/getUser")
+    public User getUser() {
+        User user = new User();
+        user.setUserName("Bob");
+        user.setPassWord("Thisisbobpwd");
+        return user;
     }
 }
